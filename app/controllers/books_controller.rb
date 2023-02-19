ccae3b5ffe4d56ce7a9f304e58922683c4c2612d
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-02-19 09:23:21 +0000

    [Finish]Bookers1-debug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,7 +27,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
 
     if @book.update(book_params)
-      flash[:notice] = "Book was successfully updated."
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       render :edit
